4ce4b7cb5ce07c804eeba67055d8660371c7b83d
shouya0331 <USER_INFO@users.noreply.github.com>
2020-07-23 15:48:10 +0900

    input要素を生成しましょう

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -9,6 +9,13 @@ if (window.location.pathname === '/messages/new') {
       // 表示する画像を生成
       const blobImage = document.createElement('img')
       blobImage.setAttribute('src', blob)
+
+      // ファイル選択ボタンを生成
+      const inputHTML = document.createElement('input')
+      inputHTML.setAttribute('id', `message_images_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'message[images][]')
+      inputHTML.setAttribute('type', 'file')
+
       // 生成したHTMLの要素をブラウザに表示させる
       imageElement.appendChild(blobImage)
       ImageList.appendChild(imageElement)
